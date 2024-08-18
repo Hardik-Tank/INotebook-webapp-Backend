ce007b3e03e83cd83bf317c9af86b6ae3ca169cc
Hardik-Tank <122682950+Hardik-Tank@users.noreply.github.com>
2024-08-18 23:23:20 +0530

    change the cors file

--- index.js
+++ index.js
@@ -8,31 +8,7 @@ connectToMongo();
 const app = express();
 const port = process.env.PORT || 5000; // Use a default port if not set in the environment
 
-var whitelist = ["https://inotebooklibrary.vercel.app"]; //white list consumers
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
-  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-Requested-With",
-    "device-remember-token",
-    "Access-Control-Allow-Origin",
-    "Origin",
-    "Accept",
-  ],
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 
 // Available Routes
